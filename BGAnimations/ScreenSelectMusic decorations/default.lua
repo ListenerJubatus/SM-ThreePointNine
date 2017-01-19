a4ae8470e4aa6d81a9bcde666ba0d87e48ff5a69
ListenerJubatus <superfox436@gmail.com>
2017-01-19 00:41:40 -0600

    How can you NOT have CDtitles in a 3.9 recreation?!

--- BGAnimations/ScreenSelectMusic decorations/default.lua
+++ BGAnimations/ScreenSelectMusic decorations/default.lua
@@ -101,49 +101,35 @@ t[#t+1] = Def.ActorFrame {
 	};
 };
 
-if GAMESTATE:IsCourseMode() then
-t[#t+1] = LoadActor("_courseframe") .. {
-		InitCommand=cmd(x,SCREEN_CENTER_X-160;y,SCREEN_CENTER_Y-88;draworder,80;visible,GAMESTATE:IsCourseMode(););
-		OffCommand=cmd(bouncebegin,0.5;addx,-SCREEN_WIDTH*0.6);
-		OnCommand=cmd(addx,-SCREEN_WIDTH*0.6;bounceend,0.5;addx,SCREEN_WIDTH*0.6;);
-		};
+if not GAMESTATE:IsCourseMode() then
+	local function CDTitleUpdate(self)
+		local song = GAMESTATE:GetCurrentSong();
+		local cdtitle = self:GetChild("CDTitle");
+		local height = cdtitle:GetHeight();
 		
-t[#t+1] = StandardDecorationFromFileOptional("SongTime","SongTime") .. {
-	SetCommand=function(self)
-		local curSelection = nil;
-		local length = 0.0;
-		if GAMESTATE:IsCourseMode() then
-			curSelection = GAMESTATE:GetCurrentCourse();
-			self:playcommand("Reset");
-			if curSelection then
-				self:settext("");
+		if song then
+			if song:HasCDTitle() then
+				cdtitle:visible(true);
+				cdtitle:Load(song:GetCDTitlePath());
+			else
+				cdtitle:visible(false);
 			end;
 		else
-			curSelection = GAMESTATE:GetCurrentSong();
-			self:playcommand("Reset");
-			if curSelection then
-				length = curSelection:MusicLengthSeconds();
-				if curSelection:IsLong() then
-					self:playcommand("Long");
-				elseif curSelection:IsMarathon() then
-					self:playcommand("Marathon");
-				else
-					self:playcommand("Reset");
-				end
-			else
-				length = 0.0;
-				self:playcommand("Reset");
-			end;
-			self:settext( SecondsToMSS(length) );
+			cdtitle:visible(false);
 		end;
+		
+		self:zoom(scale(height,32,480,1,32/480))
 	end;
-	CurrentSongChangedMessageCommand=cmd(playcommand,"Set");
-	CurrentCourseChangedMessageCommand=cmd(playcommand,"Set");
-	CurrentTrailP1ChangedMessageCommand=cmd(playcommand,"Set");
-	CurrentTrailP2ChangedMessageCommand=cmd(playcommand,"Set");
-};
+	t[#t+1] = Def.ActorFrame {
+		OnCommand=cmd(draworder,105;x,SCREEN_CENTER_X-64;y,SCREEN_CENTER_Y-87;zoom,0;sleep,0.5;decelerate,0.25;zoom,1;SetUpdateFunction,CDTitleUpdate);
+		OffCommand=cmd(bouncebegin,0.5;addx,-SCREEN_WIDTH*0.6);
+		Def.Sprite {
+			Name="CDTitle";
+			OnCommand=cmd(draworder,106;shadowlength,1;zoom,0.75;diffusealpha,1;zoom,0;bounceend,0.35;zoom,0.75;spin;effectperiod,2;effectmagnitude,0,90,0);
+			BackCullCommand=cmd(diffuse,color("0.5,0.5,0.5,1"));
+		};	
+	};
 end;
-
 	
 -- Difficulty frames
 
@@ -223,6 +209,74 @@ t[#t+1] = LoadFont("_neuropol 36px") .. {
 			end
           end
 };
+
+if GAMESTATE:IsCourseMode() then
+
+t[#t+1] = LoadActor("_courseframe") .. {
+		InitCommand=cmd(x,SCREEN_CENTER_X-160;y,SCREEN_CENTER_Y-88;draworder,80;visible,GAMESTATE:IsCourseMode(););
+		OffCommand=cmd(bouncebegin,0.5;addx,-SCREEN_WIDTH*0.6);
+		OnCommand=cmd(addx,-SCREEN_WIDTH*0.6;bounceend,0.5;addx,SCREEN_WIDTH*0.6;);
+		};
+		
+t[#t+1] = Def.ActorFrame {
+	InitCommand=cmd(draworder,80;);
+	StandardDecorationFromFileOptional("SongTime","SongTime") .. {
+	SetCommand=function(self)
+		local curSelection = nil;
+		local length = 0.0;
+		if GAMESTATE:IsCourseMode() then
+			curSelection = GAMESTATE:GetCurrentCourse();
+			self:playcommand("Reset");
+			if curSelection then
+				self:settext("");
+			end;
+		else
+			curSelection = GAMESTATE:GetCurrentSong();
+			self:playcommand("Reset");
+			if curSelection then
+				length = curSelection:MusicLengthSeconds();
+				if curSelection:IsLong() then
+					self:playcommand("Long");
+				elseif curSelection:IsMarathon() then
+					self:playcommand("Marathon");
+				else
+					self:playcommand("Reset");
+				end
+			else
+				length = 0.0;
+				self:playcommand("Reset");
+			end;
+			self:settext( SecondsToMSS(length) );
+		end;
+	end;
+	CurrentSongChangedMessageCommand=cmd(playcommand,"Set");
+	CurrentCourseChangedMessageCommand=cmd(playcommand,"Set");
+	CurrentTrailP1ChangedMessageCommand=cmd(playcommand,"Set");
+	CurrentTrailP2ChangedMessageCommand=cmd(playcommand,"Set");
+	};
+
+	Def.ActorFrame {
+		OnCommand=cmd(diffusealpha,0;sleep,2.0;smooth,0.4;diffusealpha,1;);
+		LoadFont("_numbers2") .. { 
+          InitCommand=cmd(x,SCREEN_CENTER_X-220-96;y,SCREEN_CENTER_Y-78;horizalign,center;diffuse,color("#FFFFFF");strokecolor,Color("Outline");zoom,1.0;diffusealpha,0;); 
+          CurrentCourseChangedMessageCommand=cmd(queuecommand,"Set"); 
+          ChangedLanguageDisplayMessageCommand=cmd(queuecommand,"Set"); 
+		  OnCommand=cmd(smooth,0.2;diffusealpha,1;);
+		  OffCommand=cmd(decelerate,0.3;diffusealpha,0;);
+          SetCommand=function(self) 
+               local course = GAMESTATE:GetCurrentCourse(); 
+               if course then
+                    self:settext(course:GetEstimatedNumStages()); 
+                    self:queuecommand("Refresh");
+				else
+					self:settext("");
+					self:queuecommand("Refresh"); 	
+               end 
+          end; 
+		};
+	};
+	};
+end;
 	
 t[#t+1] = StandardDecorationFromFileOptional("StageDisplay","StageDisplay");
 t[#t+1] = StandardDecorationFromFileOptional("BPMDisplay","BPMDisplay");
@@ -238,7 +292,7 @@ t[#t+1] = StandardDecorationFromFileOptional("SongOptions","SongOptionsText") ..
 t[#t+1] = Def.ActorFrame{
 	Def.Quad{
 		InitCommand=cmd(draworder,99;FullScreen;diffuse,color("0,0,0,1");diffusealpha,0);
-		ShowPressStartForOptionsCommand=cmd(linear,0.2;diffusealpha,1);
+		ShowPressStartForOptionsCommand=cmd(linear,0.8;diffusealpha,1);
 	};
 };
 	
